8dca3e913d3fd5c8c4833afac0b5e4371b9b52de
Waldir Júnior <waldirjunior.gpn@gmail.com>
2024-11-23 15:27:55 -0300

    Libera o CORS para requisições de qualquer origem

--- src/main/java/com/gratiStore/api_gratiStore/infra/config/WebConfig.java
+++ src/main/java/com/gratiStore/api_gratiStore/infra/config/WebConfig.java
@@ -10,9 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false);
     }
 }
