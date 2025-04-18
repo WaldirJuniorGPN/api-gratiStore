115491aac303db8df2608b639450b8fda1077a27
Waldir Júnior <waldirjunior.gpn@gmail.com>
2025-04-18 08:35:58 -0300

    adiciona o repository

--- src/main/java/com/gratiStore/api_gratiStore/infra/repository/PontoEletronicoRepository.java
+++ src/main/java/com/gratiStore/api_gratiStore/infra/repository/PontoEletronicoRepository.java
@@ -1,8 +1,6 @@
 package com.gratiStore.api_gratiStore.infra.repository;
 
 import com.gratiStore.api_gratiStore.domain.entities.ponto.PontoEletronico;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -10,6 +8,4 @@ import java.util.Optional;
 public interface PontoEletronicoRepository extends JpaRepository<PontoEletronico, Long> {
 
     Optional<PontoEletronico> findById(Long id);
-
-    Optional<Page<PontoEletronico>> findPageAll(Pageable pageable);
 }
