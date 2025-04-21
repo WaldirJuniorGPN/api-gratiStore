c52f0befac60100f7812042ac764312e1f3b769f
Waldir Júnior <waldirjunior.gpn@gmail.com>
2025-04-21 14:45:26 -0300

    adiciona configuração para o Testcontainers

---
+++ src/test/java/com/gratiStore/api_gratiStore/infra/config/TestContainerConfig.java
@@ -0,0 +1,28 @@
+package com.gratiStore.api_gratiStore.infra.config;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+public abstract class TestContainerConfig {
+
+    @Container
+    protected static final MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0")
+            .withDatabaseName("grati_store_test")
+            .withUsername("root")
+            .withPassword("root");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+        registry.add("spring.datasource.username", mysql::getUsername);
+        registry.add("spring.datasource.password", mysql::getPassword);
+        registry.add("spring.datasource.driver-class-name", mysql::getDriverClassName);
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
+    }
+} 
