b5c92117216c208e41fb629872727b362daadbcf
Waldir Júnior <waldirjunior.gpn@gmail.com>
2025-04-26 23:24:58 -0300

    feat: implementa CalculadoraFactory para criação de calculadoras

---
+++ src/main/java/com/gratiStore/api_gratiStore/domain/factory/calculadora/impl/CalculadoraFactoryImpl.java
@@ -0,0 +1,30 @@
+package com.gratiStore.api_gratiStore.domain.factory.calculadora.impl;
+
+import com.gratiStore.api_gratiStore.controller.dto.request.calculadora.CalculadoraRequest;
+import com.gratiStore.api_gratiStore.domain.entities.calculadora.Calculadora;
+import com.gratiStore.api_gratiStore.domain.factory.calculadora.CalculadoraFactory;
+import com.gratiStore.api_gratiStore.domain.service.loja.LojaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CalculadoraFactoryImpl implements CalculadoraFactory {
+
+    private final LojaService lojaService;
+
+    @Override
+    public Calculadora criar(CalculadoraRequest request) {
+        var loja = lojaService.buscarLoja(request.lojaId());
+
+        return new Calculadora(request.nome(),
+                request.percentualPrimeiroColocado(),
+                request.percentualSegundoColocado(),
+                request.percentualTerceiroColocado(),
+                request.percentualDemaisColocados(),
+                request.bonusPrimeiroColocado(),
+                request.bonusSegundoColocado(),
+                request.bonusTerceiroColocado(),
+                loja);
+    }
+}
