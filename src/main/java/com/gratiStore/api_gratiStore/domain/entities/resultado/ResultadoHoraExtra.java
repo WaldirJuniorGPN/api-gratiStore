d5736e5af86e5cb20e7dfd0fe17cec3771fa0452
Waldir Júnior <waldirjunior.gpn@gmail.com>
2025-05-01 12:51:18 -0300

    implementa entidade ResultadoHoraExtra

---
+++ src/main/java/com/gratiStore/api_gratiStore/domain/entities/resultado/ResultadoHoraExtra.java
@@ -0,0 +1,49 @@
+package com.gratiStore.api_gratiStore.domain.entities.resultado;
+
+import com.gratiStore.api_gratiStore.domain.entities.EntidadeBase;
+import com.gratiStore.api_gratiStore.domain.entities.atendente.Atendente;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import static com.gratiStore.api_gratiStore.domain.validator.ResultadoHoraExtraValidator.*;
+
+@Entity
+@Table(name = "resultados_horas_extras", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"atendente_id", "mes", "ano"})
+})
+@Getter
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ResultadoHoraExtra extends EntidadeBase {
+
+    @ManyToOne
+    @JoinColumn(name = "atendente_id", nullable = false)
+    private Atendente atendente;
+
+    @Column(name = "mes", nullable = false)
+    private Integer mes;
+
+    @Column(name = "ano", nullable = false)
+    private Integer ano;
+
+    @Column(name = "horas_extras", nullable = false, precision = 5, scale = 2)
+    private BigDecimal horasExtras;
+
+    public ResultadoHoraExtra(Atendente atendente, Integer mes, Integer ano, BigDecimal horasExtras) {
+        validarAtendente(atendente);
+        validarMes(mes);
+        validarAno(ano);
+        validarHorasExtras(horasExtras);
+
+        this.atendente = atendente;
+        this.mes = mes;
+        this.ano = ano;
+        this.horasExtras = horasExtras.setScale(2, RoundingMode.HALF_UP);
+    }
+}
---
+++ src/main/java/com/gratiStore/api_gratiStore/domain/validator/ResultadoHoraExtraValidator.java
@@ -0,0 +1,43 @@
+package com.gratiStore.api_gratiStore.domain.validator;
+
+import com.gratiStore.api_gratiStore.domain.entities.atendente.Atendente;
+import com.gratiStore.api_gratiStore.domain.exception.ValidacaoNegocioException;
+
+import java.math.BigDecimal;
+
+public class ResultadoHoraExtraValidator {
+
+    private ResultadoHoraExtraValidator() {
+        throw new UnsupportedOperationException("Classe utilitária, não deve ser instanciada");
+    }
+
+    public static void validarAtendente(Atendente atendente) {
+        if (atendente == null) {
+            throw new ValidacaoNegocioException("O atendente não pode estar nulo");
+        }
+    }
+
+    public static void validarMes(Integer mes) {
+        if (mes == null) {
+            throw new ValidacaoNegocioException("O mês não pode estar nulo");
+        }
+        if (mes < 1 || mes > 12) {
+            throw new ValidacaoNegocioException("O mês deve estar entre 1 e 12");
+        }
+    }
+
+    public static void validarAno(Integer ano) {
+        if (ano == null) {
+            throw new ValidacaoNegocioException("O ano não pode ser nulo");
+        }
+        if (ano < 2000) {
+            throw new ValidacaoNegocioException("O ano deve ser maior que 2000");
+        }
+    }
+
+    public static void validarHorasExtras(BigDecimal horasExtras) {
+        if (horasExtras == null) {
+            throw new ValidacaoNegocioException("a hora extra não pode ser nula");
+        }
+    }
+}
