c3fb3342cc011fb020802cad51b470c281a5ac79
Waldir Júnior <waldirjunior.gpn@gmail.com>
2025-05-18 16:32:33 -0300

    adiciona validações de integridade de entrada

---
+++ src/main/java/com/gratiStore/api_gratiStore/domain/validator/integridadeDeEntrada/Validator.java
@@ -0,0 +1,45 @@
+package com.gratiStore.api_gratiStore.domain.validator.integridadeDeEntrada;
+
+import com.gratiStore.api_gratiStore.controller.dto.request.calculadora.CalculadoraRequest;
+import org.springframework.data.domain.Pageable;
+
+public class Validator {
+
+    public Validator() {
+        throw new UnsupportedOperationException("Classe utilitária, não deve ser instanciada");
+    }
+
+    public static void validarRequisicao(CalculadoraRequest request) {
+        if (request == null) {
+            throw new IllegalStateException("A requisição não pode ser nula");
+        }
+    }
+
+    public static void validarId(Long id) {
+        if (id == null) {
+            throw new IllegalStateException("O ID não pode ser nulo");
+        }
+
+        if (id < 1) {
+            throw new IllegalStateException("Não é permitido enviar um ID menor do que 1");
+        }
+    }
+
+    public static void validarPageable(Pageable pageable) {
+        if (pageable == null) {
+            throw new IllegalStateException("O objeto de paginação não pode ser nulo");
+        }
+
+        if (pageable.getPageNumber() < 0) {
+            throw new IllegalStateException("O número de páginas não pode ser negativo");
+        }
+
+        if (pageable.getPageSize() > 100) {
+            throw new IllegalStateException("O tamanho da página execede o limite máximo permitido (100)");
+        }
+
+        if (pageable.getPageSize() < 1) {
+            throw new IllegalStateException("O tamanho da página deve ser pelo menos 1");
+        }
+    }
+}
