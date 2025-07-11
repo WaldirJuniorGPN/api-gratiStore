ac0c558d2147af0624dc500e877cbd63aea8b12c
Waldir Júnior <waldirjunior.gpn@gmail.com>
2025-07-11 16:10:45 -0300

    delega responsabilidade de calcular valor a receber para a classe CalculadoraDeHorasExtras

--- src/main/java/com/gratiStore/api_gratiStore/domain/service/horasExtras/impl/HorasExtrasServiceImpl.java
+++ src/main/java/com/gratiStore/api_gratiStore/domain/service/horasExtras/impl/HorasExtrasServiceImpl.java
@@ -26,10 +26,6 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class HorasExtrasServiceImpl implements HorasExtrasService {
 
-    private static final BigDecimal DIAS_DO_MES = BigDecimal.valueOf(30);
-    private static final BigDecimal JORNADA_DIARIA = BigDecimal.valueOf(8);
-    private static final BigDecimal ADICIONAL_HORA_EXTRA = BigDecimal.valueOf(0.5);
-
     private final PontoEletronicoService pontoEletronicoService;
     private final CalculadoraDeHorasExtras calculadora;
     private final AgrupadorDePontosPorSemana agrupadorDePontosPorSemana;
@@ -44,10 +40,10 @@ public class HorasExtrasServiceImpl implements HorasExtrasService {
         var atendentes = buscarAtendentesDaLoja(request.lojaId());
         var pontosEletronicos = pontoEletronicoService.listarHistorico(atendentes, request.mes(), request.ano());
         var pontosAgrupados = agrupadorDePontosPorSemana.agrupar(pontosEletronicos);
-        Map<Atendente, Duration> totalHorasExtrasPorAtendente = calculadora.calcular(pontosAgrupados);
+        Map<Atendente, Duration> totalHorasExtrasPorAtendente = calculadora.calcularHorasExtras(pontosAgrupados);
 
         totalHorasExtrasPorAtendente.forEach((atendente, horasExtras) -> {
-            valorAReceber = calcularValorAReceber(atendente.getSalario(), horasExtras);
+            valorAReceber = calculadora.calcularValorAReceber(atendente.getSalario(), horasExtras);
             salvarNoBanco(request.mes(), request.ano(), atendente,valorAReceber, horasExtras);
         });
     }
@@ -72,15 +68,5 @@ public class HorasExtrasServiceImpl implements HorasExtrasService {
 
     private void salvarNoBanco(int mes, int ano, Atendente atendente, BigDecimal valorAReceber, Duration horasExtras) {
         repository.save(new ResultadoHoraExtra(atendente, mes, ano, valorAReceber, horasExtras));
-    }
-
-    private BigDecimal calcularValorAReceber(BigDecimal salario, Duration horasExtras) {
-
-        var salarioDia = salario.divide(DIAS_DO_MES, MathContext.DECIMAL64);
-        var valorHora = salarioDia.divide(JORNADA_DIARIA, MathContext.DECIMAL64);
-        var valorHoraExtra = valorHora.add(valorHora.multiply(ADICIONAL_HORA_EXTRA, MathContext.DECIMAL64));
-
-        return valorHoraExtra.multiply(BigDecimal.valueOf(horasExtras.toHours()))
-                .setScale(2, RoundingMode.HALF_UP);
     }
 }
