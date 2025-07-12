d2475e7937e07bd65dcf7e0ac42fdd045e66f07c
Waldir Júnior <waldirjunior.gpn@gmail.com>
2025-07-12 18:35:54 -0300

    adiciona as configurações de Transaction e Rollback na classe de configuração do TestContainer

--- src/test/java/com/gratiStore/api_gratiStore/infra/config/TestContainerConfig.java
+++ src/test/java/com/gratiStore/api_gratiStore/infra/config/TestContainerConfig.java
@@ -2,17 +2,21 @@ package com.gratiStore.api_gratiStore.infra.config;
 
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.*;
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
 
 @SpringBootTest
 @Testcontainers()
 @AutoConfigureTestDatabase(replace = NONE)
+@Transactional
+@Rollback
 public abstract class TestContainerConfig {
 
     @Container
