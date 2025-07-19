e1720f5545f2893fbae5311976db2994389d8b0b
Waldir Júnior <waldirjunior.gpn@gmail.com>
2025-07-19 11:18:12 -0300

    Implementa os testes de integração relacionados à PontoEletronicoServiceImpl

--- src/test/java/com/gratiStore/api_gratiStore/domain/service/ponto/impl/PontoEletronicoServiceIT.java
+++ src/test/java/com/gratiStore/api_gratiStore/domain/service/ponto/impl/PontoEletronicoServiceIT.java
@@ -3,11 +3,14 @@ package com.gratiStore.api_gratiStore.domain.service.ponto.impl;
 import com.gratiStore.api_gratiStore.controller.dto.request.atendente.AtendenteRequest;
 import com.gratiStore.api_gratiStore.controller.dto.request.ponto.PontoRequest;
 import com.gratiStore.api_gratiStore.controller.dto.response.atendente.AtendenteResponse;
+import com.gratiStore.api_gratiStore.controller.dto.response.ponto.HistoricoResponse;
+import com.gratiStore.api_gratiStore.controller.dto.response.ponto.PontoResponse;
 import com.gratiStore.api_gratiStore.domain.entities.loja.Loja;
 import com.gratiStore.api_gratiStore.domain.service.atendente.AtendenteService;
 import com.gratiStore.api_gratiStore.domain.service.loja.LojaService;
 import com.gratiStore.api_gratiStore.domain.service.ponto.PontoEletronicoService;
 import com.gratiStore.api_gratiStore.infra.config.TestContainerConfig;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,9 +23,16 @@ import java.time.LocalTime;
 
 import static com.gratiStore.api_gratiStore.domain.utils.FeriadoUtils.NAO;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class PontoEletronicoServiceIT extends TestContainerConfig {
+
+    private final LocalDate DATA = LocalDate.of(2025, 6, 1);
+    private final LocalTime ENTRADA = LocalTime.of(8, 0);
+    private final LocalTime INICIO_ALMOCO = LocalTime.of(11, 0);
+    private final LocalTime FIM_ALMOCO = LocalTime.of(12, 0);
+    private final LocalTime SAIDA = LocalTime.of(17, 0);
 
     @Autowired
     private AtendenteService atendenteService;
@@ -36,6 +46,7 @@ public class PontoEletronicoServiceIT extends TestContainerConfig {
     private AtendenteResponse atendenteResponse;
     private PontoRequest pontoRequest;
     private Loja loja;
+    private PontoResponse pontoResponse;
 
     @BeforeEach
     void setUp() {
@@ -43,25 +54,24 @@ public class PontoEletronicoServiceIT extends TestContainerConfig {
         lojaService.salvarNoBanco(loja);
         var atendenteRequest = new AtendenteRequest("Fulano", loja.getId(), BigDecimal.valueOf(3000));
         atendenteResponse = atendenteService.criar(atendenteRequest);
-        pontoRequest = new PontoRequest(LocalDate.of(2025, 6, 1),
-                LocalTime.of(8, 0),
-                LocalTime.of(11, 0),
-                LocalTime.of(12, 0),
-                LocalTime.of(17, 0),
+        pontoRequest = new PontoRequest(DATA,
+                ENTRADA,
+                INICIO_ALMOCO,
+                FIM_ALMOCO,
+                SAIDA,
                 NAO,
                 atendenteResponse.id());
+        pontoResponse = pontoEletronicoService.registrarPonto(pontoRequest);
     }
 
     @Test
     void deveRegistrarPonto() {
-        var resultado = pontoEletronicoService.registrarPonto(pontoRequest);
 
-        assertEquals(atendenteResponse.id(), resultado.id());
+        assertEquals(atendenteResponse.id(), pontoResponse.id());
     }
 
     @Test
     void deveListarHistorico_comRetornoPaginado() {
-        pontoEletronicoService.registrarPonto(pontoRequest);
 
         var pageable = PageRequest.of(1, 10);
         var response = pontoEletronicoService.listarHistorico(pageable);
@@ -76,5 +86,57 @@ public class PontoEletronicoServiceIT extends TestContainerConfig {
 
         assertEquals(1, pontos.size());
         assertEquals(pontoRequest.data(), pontos.get(0).getData());
+    }
+
+    @Test
+    void deveAtualizarPontoEletronico_comSucesso() {
+        var pontoId = pontoResponse.id();
+        var pontoRequestAtualizado = new PontoRequest(DATA,
+                ENTRADA,
+                INICIO_ALMOCO,
+                FIM_ALMOCO,
+                SAIDA.plusHours(1),
+                NAO,
+                atendenteResponse.id()
+        );
+
+        var resultado = pontoEletronicoService.atualizar(pontoId, pontoRequestAtualizado);
+
+        assertEquals(pontoResponse.saida().plusHours(1), resultado.saida());
+    }
+
+    @Test
+    void deveFalharAoAtualizarPontoEletronico_quandoIdNaoEncontrado() {
+        var idInvalido = 2222L;
+        var msgError = "Ponto Eletrônico com id %d não encontrado";
+        var exception = assertThrows(EntityNotFoundException.class, () -> pontoEletronicoService.atualizar(idInvalido, pontoRequest));
+        assertEquals(String.format(msgError, idInvalido), exception.getMessage());
+    }
+
+    @Test
+    void deveBuscarPontoEletronico_comSucesso() {
+        var historicoResponse = new HistoricoResponse(pontoResponse.id(),
+                pontoResponse.data(),
+                pontoResponse.entrada(),
+                pontoResponse.incioAlmoco(),
+                pontoResponse.fimAlmoco(),
+                pontoResponse.saida(),
+                pontoResponse.feriado(),
+                atendenteResponse.id());
+        var resultado = pontoEletronicoService.buscar(pontoResponse.id());
+
+        assertEquals(historicoResponse, resultado);
+    }
+
+    @Test
+    void deveFalharAoBuscarPontoEletronico_quandoIdNaoEncontrado() {
+        var idInvalido = 2222l;
+        assertThrows(EntityNotFoundException.class, () -> pontoEletronicoService.buscar(idInvalido));
+    }
+
+    @Test
+    void deveFazerDelecaoFisica_comSucesso() {
+        pontoEletronicoService.deletar(pontoResponse.id());
+        assertThrows(EntityNotFoundException.class, () -> pontoEletronicoService.buscar(pontoResponse.id()));
     }
 }
