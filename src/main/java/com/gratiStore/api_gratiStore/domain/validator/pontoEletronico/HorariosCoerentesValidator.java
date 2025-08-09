975e889433245c3c025915869ccd9f5507dbef87
Waldir Júnior <waldirjunior.gpn@gmail.com>
2025-08-09 11:14:14 -0300

    Refatora o método isValid chamando a classe utilitária HorariosValidator

--- src/main/java/com/gratiStore/api_gratiStore/domain/validator/pontoEletronico/HorariosCoerentesValidator.java
+++ src/main/java/com/gratiStore/api_gratiStore/domain/validator/pontoEletronico/HorariosCoerentesValidator.java
@@ -4,23 +4,15 @@ import com.gratiStore.api_gratiStore.controller.dto.request.ponto.PontoRequest;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
+import static com.gratiStore.api_gratiStore.domain.validator.negocio.HorariosValidator.validarHorarios;
+
 public class HorariosCoerentesValidator implements ConstraintValidator<HorariosCoerentes, PontoRequest> {
 
     @Override
     public boolean isValid(PontoRequest pontoRequest, ConstraintValidatorContext context) {
-        var entradaPreenchida = pontoRequest.entrada() != null;
-        var inicioAlmocoPreenchido = pontoRequest.inicioAlmoco() != null;
-        var fimAlmocoPreenchido = pontoRequest.fimAlmoco() != null;
-        var saidaPreenchida = pontoRequest.saida() != null;
-
-        if (!entradaPreenchida && !inicioAlmocoPreenchido && !fimAlmocoPreenchido && !saidaPreenchida) {
-            return true;
-        }
-
-        if (entradaPreenchida && inicioAlmocoPreenchido && fimAlmocoPreenchido && saidaPreenchida) {
-            return true;
-        }
-
-        return false;
+        return validarHorarios(pontoRequest.entrada(),
+                pontoRequest.inicioAlmoco(),
+                pontoRequest.fimAlmoco(),
+                pontoRequest.saida());
     }
 }
