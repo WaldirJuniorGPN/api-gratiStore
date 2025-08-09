1e39470638b4aa36f4c7ffea3e475b0199c6be44
Waldir Júnior <waldirjunior.gpn@gmail.com>
2025-08-09 10:47:19 -0300

    Adiciona a anotação personalizada @HorariosCoerentes para validar os atributos do tipo LocalTime

--- src/main/java/com/gratiStore/api_gratiStore/controller/dto/request/ponto/PontoRequest.java
+++ src/main/java/com/gratiStore/api_gratiStore/controller/dto/request/ponto/PontoRequest.java
@@ -1,6 +1,10 @@
 package com.gratiStore.api_gratiStore.controller.dto.request.ponto;
 
+import com.gratiStore.api_gratiStore.domain.utils.AtestadoUtils;
+import com.gratiStore.api_gratiStore.domain.utils.DescontarEmHorasUtils;
 import com.gratiStore.api_gratiStore.domain.utils.FeriadoUtils;
+import com.gratiStore.api_gratiStore.domain.utils.FolgaUtils;
+import com.gratiStore.api_gratiStore.domain.validator.pontoEletronico.HorariosCoerentes;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
@@ -8,6 +12,7 @@ import jakarta.validation.constraints.PastOrPresent;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+@HorariosCoerentes
 public record PontoRequest(
 
         @NotNull(message = "A data não pode ser nula")
@@ -15,24 +20,29 @@ public record PontoRequest(
         @Schema(type = "string", example = "2025-04-19", format = "date")
         LocalDate data,
 
-        @NotNull(message = "A entrada não pode ser nula")
         @Schema(type = "string", example = "08:00:00", format = "time")
         LocalTime entrada,
 
-        @NotNull(message = "O início do almoço não pode ser nulo")
         @Schema(type = "string", example = "12:00:00", format = "time")
         LocalTime inicioAlmoco,
 
-        @NotNull(message = "O fim do almoço não pode ser nulo")
         @Schema(type = "string", example = "13:00:00", format = "time")
         LocalTime fimAlmoco,
 
-        @NotNull(message = "A saída não pode ser nula")
         @Schema(type = "string", example = "17:00:00", format = "time")
         LocalTime saida,
 
-        @NotNull
+        @NotNull(message = "O status de Feriado não pode ser nulo")
         FeriadoUtils feriado,
+
+        @NotNull(message = "O status do Atestado não pode ser nulo")
+        AtestadoUtils atestado,
+
+        @NotNull(message = "O status da Folga não pode ser nulo")
+        FolgaUtils folga,
+
+        @NotNull(message = "O status de Descontar em Horas não pode ser nulo")
+        DescontarEmHorasUtils descontarEmHoras,
 
         @NotNull(message = "O ID do atendente não pode ser nulo")
         @Schema(example = "123")
